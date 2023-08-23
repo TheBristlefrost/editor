a01c58e0b2d21c59a88ec3b91af206e3c4faa8f5
Roni Nevalainen <me@bristlefrost.net>
2023-08-23 20:04:43 +0300

    feat: add bolding without selecting first (still very broken)

--- src/rich-text.ts
+++ src/rich-text.ts
@@ -6,10 +6,33 @@ function toggleBold() {
 	const selection = document.getSelection();
 	if (selection === null) return;
 
-	if (utils.selectionHasClass(selection, styles.bold)) {
-		utils.removeClassFromSelection(selection, styles.bold);
-	} else {
-		utils.addClassToSelection(selection, styles.bold);
+	if (selection.type === 'Range') {
+		if (utils.selectionHasClass(selection, styles.bold)) {
+			utils.removeClassFromSelection(selection, styles.bold);
+		} else {
+			utils.addClassToSelection(selection, styles.bold);
+		}
+	} else if (selection.type === 'Caret') {
+		const range = selection.getRangeAt(0);
+
+		if (selection.anchorNode?.parentElement?.tagName === 'SPAN' && selection.anchorNode?.parentElement?.classList?.contains(styles.bold)) {
+			const parent = selection.anchorNode.parentElement;
+
+			if (parent.textContent && range.startOffset === parent.textContent.length - 1) {
+				parent.insertAdjacentText('afterend', new DOMParser().parseFromString('&nbsp;', 'text/html').documentElement.textContent as string);
+			}
+		} else {
+			const span = document.createElement('span');
+
+			span.className = styles.bold;
+			span.appendChild(document.createTextNode(new DOMParser().parseFromString('&ZeroWidthSpace;', 'text/html').documentElement.textContent as string));
+
+			utils.insertAtCursor(span);
+
+			range.selectNodeContents(span);
+
+			span.focus();
+		}
 	}
 }
 
