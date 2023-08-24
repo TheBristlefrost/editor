941340feec4e2200699b00e4bcedc6572c7210ff
Roni Nevalainen <me@bristlefrost.net>
2023-08-24 18:08:32 +0300

    fix: make disabling bold work

--- src/rich-text.ts
+++ src/rich-text.ts
@@ -18,8 +18,15 @@ function toggleBold() {
 		if (selection.anchorNode?.parentElement?.tagName === 'SPAN' && selection.anchorNode?.parentElement?.classList?.contains(styles.bold)) {
 			const parent = selection.anchorNode.parentElement;
 
-			if (parent.textContent && range.startOffset === parent.textContent.length - 1) {
-				parent.insertAdjacentText('afterend', new DOMParser().parseFromString('&nbsp;', 'text/html').documentElement.textContent as string);
+			if (parent.textContent && (range.startOffset === parent.textContent.length)) {
+				const spanElement = document.createElement('span');
+				spanElement.appendChild(document.createTextNode(new DOMParser().parseFromString('&ZeroWidthSpace;', 'text/html').documentElement.textContent as string));
+
+				parent.insertAdjacentElement('afterend', spanElement);
+
+				range.setStartAfter(spanElement);
+				range.setEndAfter(spanElement);
+				range.collapse(true);
 			}
 		} else {
 			const span = document.createElement('span');
